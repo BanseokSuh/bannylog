23590cd7f0aac96361c059ccd0ace9d3c01b8fea
BanseokSuh <still3028@gmail.com>
2023-04-19 07:28:29 +0900

    first commit

---
+++ src/main/java/com/bannylog/BannylogApplication.java
@@ -0,0 +1,13 @@
+package com.bannylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannylogApplication.class, args);
+	}
+
+}
