d1728117a5b7dce519a09bcca8f9ce2a91a6446a
BanseokSuh <still3028@gmail.com>
2023-04-22 23:23:57 +0900

    글 저장 api 및 테스트 코드

--- src/main/java/com/bannylog/api/domain/Post.java
+++ src/main/java/com/bannylog/api/domain/Post.java
@@ -1,11 +1,13 @@
 package com.bannylog.api.domain;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Post {
     @Id
--- src/test/java/com/bannylog/api/controller/PostControllerTest.java
+++ src/test/java/com/bannylog/api/controller/PostControllerTest.java
@@ -1,15 +1,15 @@
 package com.bannylog.api.controller;
 
-import com.bannylog.api.repository.PostRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MockMvc;
+import com.bannylog.api.repository.PostRepository;
+import org.springframework.http.MediaType;
+import com.bannylog.api.domain.Post;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -27,6 +27,11 @@ class PostControllerTest {
 
     @Autowired
     private PostRepository postRepository;
+
+    @BeforeEach // 매 테스트 시 실행되는 함수 정의
+    void clean() {
+        postRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("/posts 요청 시 Hello World를 출력한다.")
@@ -66,6 +71,9 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청 시 DB에 값이 저장된다.")
     void test4() throws Exception {
+        // before
+
+
         // when
         mockMvc.perform(post("/posts")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -75,5 +83,9 @@ class PostControllerTest {
                 .andDo(print());
         // then
         assertEquals(1L, postRepository.count());
+
+        Post post = postRepository.findAll().get(0);
+        assertEquals("제목입니다.", post.getTitle());
+        assertEquals("내용입니다.", post.getContent());
     }
 }
