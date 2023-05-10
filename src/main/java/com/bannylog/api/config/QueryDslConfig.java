2a1f810ede8e7e14e7f9bf36b538acd1a97521ad
BanseokSuh <62047302+BanseokSuh@users.noreply.github.com>
2023-05-10 13:31:17 +0900

    querydslconfig 어노테이션 설명

--- src/main/java/com/bannylog/api/config/QueryDslConfig.java
+++ src/main/java/com/bannylog/api/config/QueryDslConfig.java
@@ -9,10 +9,10 @@ import javax.persistence.PersistenceContext;
 
 @Configuration
 public class QueryDslConfig {
-    @PersistenceContext
+    @PersistenceContext // EntityManager를 빈으로 주입할 때 사용하는 어노테이션
     public EntityManager em;
 
-    @Bean
+    @Bean // 직접 제어가 불가능한 외부 라이브러리등을 Bean으로 만들려할 때 사용
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(em);
     }
