b8381b72879b2e9ac326bb5a9b80d716a7674572
BanseokSuh <still3028@gmail.com>
2023-06-01 00:03:33 +0900

    front(vue3) 소스코드

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// const axios from 'axios';
+
+const title = ref('')
+const content = ref('')
+
+const write = function () {
+  alert(title.value + ' / ' + content.value)
+
+  //   axios.get('http://google.com')
+}
+</script>
+
+<template>
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력해주세요" />
+  </div>
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15" />
+  </div>
+  <div class="mt-2">
+    <el-button type="primary" @click="write">글 작성 완료</el-button>
+  </div>
+</template>
+
+<style></style>
